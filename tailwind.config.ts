c22c5e2cb4df9466062e31da99c71f61df03a55f
Tobias Roeske <t.roeske91@gmail.com>
2024-09-24 15:53:42 +0200

    🔰 project setup (tailwind, eslint, fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: 'rgba(61, 207, 182, 1)',
+          light: 'rgba(61, 207, 182, 0.1)'
+        },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
-    },
+      fontFamily: {
+        karla: ['var(--font-karla)'],
+        fira: ['var(--font-fira)']
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
