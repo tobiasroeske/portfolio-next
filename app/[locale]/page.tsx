4614461244a1b8ab8e1724c49f2e6f9b984cf4bd
Tobias Roeske <t.roeske91@gmail.com>
2024-10-15 15:02:38 +0200

    :sparkles: feat: changed favicon

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
+import AboutMe from '@/components/about-me'
 import Hero from '@/components/hero'
 
 export default async function Page() {
   return (
     <>
       <Hero />
+      <AboutMe />
     </>
   )
 }
